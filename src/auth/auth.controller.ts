7eb3469229746b662a951d25be9e9c513389c52a
Gianmarcos <gians96@gmail.com>
2025-02-03 15:29:14 -0500

    [ADD] create user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,27 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
-  @MessagePattern('auth-register-user')
-  registerUser() {
-    return 'registerUser';
+  @MessagePattern('auth.register.user')
+  registerUser(@Payload() registerUserDto: RegisterUserDto) {
+    // return registerUserDto;
+    return this.authService.registerUser(registerUserDto);
+  }
+
+  @MessagePattern('auth.login.user')
+  LoginUser(@Payload() loginUserDto: LoginUserDto) {
+    return loginUserDto;
     // return this.authService.registerUser(data);
   }
 
-  LoginUser() {
-    return 'LoginUser';
-    // return this.authService.registerUser(data);
-  }
-
+  @MessagePattern('auth.verify.token.user')
   verifyTokenUser() {
-    return 'verifyTokenUser';
+    return 'verifyTokenUsersss';
     // return this.authService.registerUser(data);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import { stat } from 'fs';
+import { RegisterUserDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends PrismaClient implements OnModuleInit {
+    private readonly logger = new Logger("AuthService");
+    onModuleInit() {
+        this.$connect();
+        this.logger.log("MongoDB connected");
+    }
+
+    async registerUser(registerUserDto: RegisterUserDto) {
+        try {
+            console.log(registerUserDto);
+            
+            const { name, email, password } = registerUserDto;
+            const user = await this.user.findUnique({
+                where: { email }
+            });
+            if (user) {
+                throw new RpcException({
+                    statusCode: 400,
+                    error: "Email ya existe",
+                    message: "El email ya existe"
+                });
+            }
+            const newUser = await this.user.create({
+                data: {
+                    name,
+                    email,
+                    password
+                }
+            });
+            return { newUser, token: "123456789" }
+        } catch (error) {
+            throw new RpcException({
+                statusCode: 400,
+                error: error.message
+            });
+        }
+    }
+}
